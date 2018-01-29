e216ea5b980d7228857c0097bdf5eefb57d4a64d
isma91 <ismaydogmus@gmail.com>
2018-01-29 13:48:02 +0100

    adding nearly finish register function

--- controller/UserController.php
+++ controller/UserController.php
@@ -42,7 +42,72 @@ class UserController
     /**
      *
      */
-    public function add() {
+    public function register() {
+        $errField = array();
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $password2 = $_POST["password2"];
+        $allField = [
+            'firstname' => $firstname,
+            'lastname' => $lastname,
+            'login' => $username,
+            'email' => $email,
+            'pass' => $pass,
+            'pass2' => $pass2
+        ];
+        foreach($allField as $filed => $value) {
+            if (empty(trim($value))) {
+                array_push($errField, $value);
+            }
+        }
+        $user = new User();
+        /*
+        use the checkduplicateemail & username with user class
+        */
+        $view = new View("site#register");
+        $message = new Message();
+        $messages = $message->getMessages();
+        if (count($errField) > 0) {
+            $fields = "";
+            foreach($errField as $key => $value) {
+                $fields = $fields . ", " . $value;
+            }
+            $fields = substr($fields, 2);
+            $view->set("error", sprintf($messages["error"]["emptyField"], $fields));
+            $view->render();
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $view->set("error", $messages["error"]["emailNotValid"]);
+            $view->render();
+        } elseif (strlen($pass) <= 3 || strlen($pass2) <= 3) {
+            $view->set("error", $messages["error"]["passwordNotLong"]);
+            $view->render();
+        } elseif ($pass !== $pass2) {
+            $view->set("error", $messages["error"]["passwordNotSame"]);
+            $view->render();
+        } elseif (strlen($username) <= 3) {
+            $view->set("error", $messages["error"]["usernameNotLong"]);
+            $view->render();
+        } elseif ($field = "duplicateEmail") {
+            foreach ($allField as $field => $value) {
+                $view->set($field, $value);
+            }
+            $view->set("error", $messages["error"]["duplicateEmail"]);
+            $view->render();
+        } elseif ($field = "duplicateUsername") {
+            foreach ($allField as $field => $value) {
+                $view->set($field, $value);
+            }
+            $view->set("error", $messages["error"]["duplicateUsername"]);
+            $view->render();
+        } else {
+            $add = $user->add($firstname, $lastname, $username, $email, $password);
+            var_dump($add); die();
+            $view->set("success", $messages["success"]["register"]);
+            $view->render();
+        }
     }
 
 }
