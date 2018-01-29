a31a5e577e5cc573d46eecdef0be1eccc3b51d61
isma91 <ismaydogmus@gmail.com>
2018-01-29 13:48:15 +0100

    adding more messages

--- model/Message.php
+++ model/Message.php
@@ -20,7 +20,6 @@ class Message
      */
     public function getMessages()
     {
-        $config = new Config();
         return $this->messages;
     }
 
@@ -29,14 +28,23 @@ class Message
      */
     public function __construct()
     {
+        $config = new Config();
+        $emailTo = $config->getEmailTo();
         $errorMessage = array(
+            "emptyField" => "The following field are empty: %s !!",
+            "emailNotValid" => "Your email is not valid !!",
+            "passwordNotLong" => "The two password fields must be at least 4 characters long !!",
+            "passwordNotSame" => "The two password field must be the same !!",
+            "usernameNotLong" => "The username must be at least 4 characters long !!",
+            "duplicateEmail" => "Email already taken !!",
+            "duplicateUsername" => "Username already taken !!",
+            "somethingGotWrong" => "Something got wrong when we try to %s please send email to '" . $emailTo . "' !!",
         );
         $successMessage = array(
-            "adminCreated" => "Vous avez ajouté l'admin %s avec succès.",
-            "updateColor" => "Couleur %s modifier avec succès.",
+            "register" => "You successfully registered yourself !! You can now log in !!",
         );
         $infoMessage = array(
-            "maintenance" => "Le site affiche qu'il est en maintenance."
+            "maintenance" => "The site is in maintenance, come back later or send email to '" . $emailTo . "' !!",
         );
         $this->messages = array("error" => $errorMessage, "success" => $successMessage, "info" => $infoMessage);
     }
