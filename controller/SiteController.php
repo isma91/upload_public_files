488583164d6282747281baef056862e0a551e9a9
isma91 <ismaydogmus@gmail.com>
2018-01-29 10:46:31 +0100

    adding the Sitecontroller

---
+++ controller/SiteController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace controller;
+
+use model\Bdd;
+use model\Config;
+use model\Mail;
+use model\Message;
+use model\View;
+
+class SiteController
+{
+    public function checkMaintenance($page, array $array = array())
+    {
+        $config = new Config();
+        if ($config->getMaintenance() === "true") {
+            $view = new View("site#maintenance");
+            $view->render();
+        } else {
+            $view = new View($page);
+            if (!empty($array)) {
+                foreach ($array as $item => $value) {
+                    $view->set($item, $value);
+                }
+            }
+            $view->render();
+        }
+    }
+
+    public function index()
+    {
+        $this->checkMaintenance("site#index");
+    }
+}
