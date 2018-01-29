388bf1bf49618305914310941912b18205da5c10
isma91 <ismaydogmus@gmail.com>
2018-01-29 10:44:25 +0100

    adding the sample of the config file

---
+++ config-sample.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This is a sample of the config file, rename it to config.php
+ *
+ */
+return [
+    'host'        => 'localhost',
+    'dbname'      => 'uploadPublicFiles',
+    'user'        => 'root',
+    'password'    => '',
+    'emailTo'      => 'your_pro_email@mail.com',
+    'maintenance' => 'false'
+];
