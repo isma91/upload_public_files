57bc3f8879b4a44abd2d55101164dc79f1390456
isma91 <ismaydogmus@gmail.com>
2018-01-29 13:53:02 +0100

    adding the div for message + if error from controler we get all the date in the field

--- view/site/register.php
+++ view/site/register.php
@@ -17,33 +17,39 @@
             <h2>Please full in the form to create an account</h2>
         </div>
     </div>
+    <div class="row failed">
+        <?php if ($this->error !== null) { echo $this->error; } ?>
+    </div>
+    <div class="row success">
+        <?php if ($this->success !== null) { echo $this->success; } ?>
+    </div>
     <div class="container">
         <form id="registerForm" action="/register" method="POST" class="row">
             <div class="row">
                 <div class="input-field col s12">
                     <i class="material-icons prefix">account_circle</i>
-                    <input id="firstname" name="firstname" type="text">
+                    <input id="firstname" name="firstname" type="text" value="<?php echo $this->firstname ?>">
                     <label for="firstname">Firstname</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
                     <i class="material-icons prefix">account_circle</i>
-                    <input id="lastname" name="lastname" type="text">
+                    <input id="lastname" name="lastname" type="text" value="<?php echo $this->lastname ?>">
                     <label for="lastname">Lastname</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
                     <i class="material-icons prefix">face</i>
-                    <input id="username" name="username" type="text">
+                    <input id="username" name="username" type="text" value="<?php echo $this->username ?>">
                     <label for="username">Username</label>
                 </div>
             </div>
             <div class="row">
                 <div class="input-field col s12">
                     <i class="material-icons prefix">email</i>
-                    <input id="email" name="email" type="email">
+                    <input id="email" name="email" type="email" value="<?php echo $this->email ?>">
                     <label for="email">Email</label>
                 </div>
             </div>
@@ -51,7 +57,7 @@
                 <div class="input-field col s12">
                     <i class="material-icons prefix">vpn_key</i>
                     <i class="material-icons right" id="displayUserPass">visibility</i>
-                    <input id="pass" name="password" type="password">
+                    <input id="pass" name="password" type="password" value="<?php echo $this->password ?>">
                     <label for="pass">Password</label>
                 </div>
             </div>
@@ -59,7 +65,7 @@
                 <div class="input-field col s12">
                     <i class="material-icons prefix">vpn_key</i>
                     <i class="material-icons right" id="displayUserPass2">visibility</i>
-                    <input id="pass2" name="password2" type="password">
+                    <input id="pass2" name="password2" type="password" value="<?php echo $this->password2 ?>">
                     <label for="pass2">Rewrite your Password</label>
                 </div>
             </div>
