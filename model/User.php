09662738141c1fbfe87bac3251ef2029480d24d8
isma91 <ismaydogmus@gmail.com>
2018-01-29 12:56:34 +0100

    adding the User model

---
+++ model/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* User.php
+*
+ * Model of the User
+*
+ * PHP 7.0.8
+*
+ * @category Model
+* @author   isma91 <ismaydogmus@gmail.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+namespace model;
+class User
+{
+    
+}
