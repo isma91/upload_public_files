cb3cf1a2f4a212fa1ad91e420d3dee857957958c
isma91 <ismaydogmus@gmail.com>
2018-01-30 08:09:52 +0100

    redirect if an user is connected

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -27,14 +27,45 @@ class SiteController
     }
 
     public function index() {
-        $this->checkMaintenance("site#index");
+        $this->redirectIfLoged("site#index");
     }
     
     public function register() {
-        $this->checkMaintenance("site#register");
+        $this->redirectIfLoged("site#register");
     }
     
     public function forgotPass() {
-        $this->checkMaintenance("site#forgotPass");
+        $this->redirectIfLoged("site#forgotPass");
+    }
+
+    /**
+     * @param $viewName
+     * @param array $arraySet
+     */
+    public function redirectIfLoged($viewName, $arraySet = array()) {
+        $this->checkMaintenance($viewName);
+        $userClass = new User();
+        $connected = User::isConnected();
+        $message = new Message();
+        $messages = $message->getMessages();
+        if ($connected === true) {
+            $view = new View("user#home");
+            $user = $userClass->getUser();
+            if ($user !== false) {
+                foreach ($user as $name => $value) {
+                    $view->set($name, $value);
+                }
+            }
+            $view->set("error", $messages["error"]["mustNotBeConnected"]);
+            $view->render();
+        } else {
+            $view = new View($viewName);
+            if (!empty($arraySet)) {
+                foreach ($arraySet as $name => $value) {
+                    $view->set($name, $value);
+                }
+            }
+            $view->render();
+        }
     }
 }
