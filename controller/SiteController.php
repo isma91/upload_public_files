10393175b2f2f36aa30412590839f8b74058a674
isma91 <ismaydogmus@gmail.com>
2018-01-30 08:17:58 +0100

    adding User & fix the double view problem

--- controller/SiteController.php
+++ controller/SiteController.php
@@ -7,6 +7,7 @@ use model\Config;
 use model\Mail;
 use model\Message;
 use model\View;
+use model\User;
 
 class SiteController
 {
@@ -14,14 +15,6 @@ class SiteController
         $config = new Config();
         if ($config->getMaintenance() === "true") {
             $view = new View("site#maintenance");
-            $view->render();
-        } else {
-            $view = new View($page);
-            if (!empty($array)) {
-                foreach ($array as $item => $value) {
-                    $view->set($item, $value);
-                }
-            }
             $view->render();
         }
     }
