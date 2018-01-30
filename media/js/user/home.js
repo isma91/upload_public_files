33b6d3a93486daa5831b2bd32ef9103b012fe3ee
isma91 <ismaydogmus@gmail.com>
2018-01-30 08:32:14 +0100

    remove the error toast if it's for the redirect

---
+++ media/js/user/home.js
@@ -0,0 +1,12 @@
+/*jslint browser: true, node : true*/
+/*jslint devel : true*/
+/*global $, document, this*/
+$(document).ready(function () {
+    var errorDiv = $(".row").filter(".failed").text();
+    errorDiv = $.trim(errorDiv);
+    if (errorDiv == "You can't go here if you're connected !!") {
+        setTimeout(function () {
+            $(".row").filter(".failed").text("");
+        }, 2000);
+    }
+});
