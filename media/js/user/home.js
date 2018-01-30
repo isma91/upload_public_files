72c7fac6e963a1a2a237659563df1777eddc6713
isma91 <ismaydogmus@gmail.com>
2018-01-30 15:19:16 +0100

    using materialize select

--- media/js/user/home.js
+++ media/js/user/home.js
@@ -2,6 +2,7 @@
 /*jslint devel : true*/
 /*global $, document, this*/
 $(document).ready(function () {
+    $('select').material_select();
     var errorDiv = $(".row").filter(".failed").text();
     errorDiv = $.trim(errorDiv);
     if (errorDiv == "You can't go here if you're connected !!") {
