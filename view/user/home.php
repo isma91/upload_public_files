79cac49c96e8cdb88e59cab342640c2688b9dde3
isma91 <ismaydogmus@gmail.com>
2018-01-30 16:12:21 +0100

    get the logout button + select

--- view/user/home.php
+++ view/user/home.php
@@ -23,6 +23,32 @@
         <?php if ($this->success !== null) { echo $this->success; } ?>
     </div>
     <div class="container">
+        <form id="loginForm" action="/logout" method="POST" class="row">
+            <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" >
+            <div class="row center">
+                <button class="btn waves-effect waves-red" type="submit">Logout</button>
+            </div>
+        </form>
+    </div>
+    <div class="container">
+        <div class="row">
+            <form id="createFolderForm" action="/createFolder" method="POST" class="col s12">
+                <div class="row">
+                    <h4 class="center">You can create a folder here</h4>
+                    <div class="input-field">
+                        <i class="material-icons prefix">folder</i>
+                        <input id="folder" name="folder" type="text" >
+                        <label for="folder">Folder Name</label>
+                    </div>
+                </div>
+                <div class="row input-field">
+                    <select name="target" id="target">
+                        <option value="1">Option 1</option>
+                    </select>
+                    <label for="target">Materialize Select</label>
+                </div>
+            </form>
+        </div>
     </div>
 </body>
 </html>
